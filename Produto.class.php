1d6ad49106768fbd62ce0470290b5de0f797041b
Joaquim Martins Scavone <joaquim.scavone@ifto.edu.br>
2022-09-21 15:06:14 -0300

    Aula 03

--- Produto.class.php
+++ Produto.class.php
@@ -3,18 +3,49 @@
 
 class Produto{
     public $descricao;
-    public $valor;
-    public $estoque;
+    private $valor;
+    protected $estoque = 0;
 
-    public function addEstoque($quantidade){
+    public function setEstoque($quantidade){
         $this->estoque +=$quantidade;
     }
 
+    public function getEstoque()
+    {
+        return $this->estoque;
+    }
+    /**
+     * Registra a venda de um produto;
+     * @param int $quantidade quantidade do produto que foi vendido
+     * @return void
+     */
     public function venda($quantidade){
         $this->estoque -=$quantidade;
     }
 
+    /**
+     * Atualiza o preço com base em um percentual
+     * @param float $percentual percentual que se deseja valorizar
+     *  ex: 55.5 para 55,5% de reajuste;
+     */
     public function atualizarPreco($percentual){
        $this->valor += $this->valor*$percentual/100;
     }
+
+   
+	/**
+     * retorna o valor do produto;
+	 * @return float
+	 */
+	function getValor() {
+		return $this->valor;
+	}
+	/**
+	 * @param mixed $valor 
+	 * @return Produto
+	 */
+	function setValor($valor): self {
+		$this->valor = $valor;
+		return $this;
+	}
 }
--- index.php
+++ index.php
@@ -8,18 +8,20 @@ class Pagina
     {
         $produto = new Produto;
         $produto->descricao = 'Caixa Chocolate Nestle';
-        $produto->valor = 12.39;
-        $produto->estoque = 30;
+        $produto->setValor(12.39);
+        $produto->setEstoque(30);
         $this->printProduto($produto);
         $produto->venda(5);
+        // tentativa de acesso direto da erro!
+        // $produto->estoque -=1;
         $this->printProduto($produto);
         
         // Produto 2
         
         $produto2 = new Produto;
         $produto2->descricao = 'Cajuzinho Nestle';
-        $produto2->valor = 0.5;
-        $produto2->estoque = 15;
+        $produto2->setValor(0.5);
+        $produto2->setEstoque(15);
   
         $this->printProduto($produto2);
         $produto2->atualizarPreco(50);
@@ -28,8 +30,8 @@ class Pagina
 
     public function printProduto(Produto $p){
         echo "<h3>{$p->descricao}</h3>";
-        echo "<h5>Valor: {$p->valor}</h5>";
-        echo "<h5>Quantidade: {$p->estoque}</h5><hr>";
+        echo "<h5>Valor: {$p->getValor()}</h5>";
+        echo "<h5>Quantidade: {$p->getEstoque()}</h5><hr>";
     }
 }
 
