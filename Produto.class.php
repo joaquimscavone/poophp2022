64dbd1b8842d6b5f72bc25b2c9c19e1d9ea63664
Joaquim Martins Scavone <joaquim.scavone@ifto.edu.br>
2022-09-21 13:32:56 -0300

    Aula 02

--- Produto.class.php
+++ Produto.class.php
@@ -5,4 +5,16 @@ class Produto{
     public $descricao;
     public $valor;
     public $estoque;
+
+    public function addEstoque($quantidade){
+        $this->estoque +=$quantidade;
+    }
+
+    public function venda($quantidade){
+        $this->estoque -=$quantidade;
+    }
+
+    public function atualizarPreco($percentual){
+       $this->valor += $this->valor*$percentual/100;
+    }
 }
--- index.php
+++ index.php
@@ -2,17 +2,37 @@
 
 require_once 'Produto.class.php';
 
-$produto = new Produto;
-$produto->descricao = 'Caixa Chocolate Nestle';
-$produto->valor = 12.39;
-$produto->estoque = 30;
+class Pagina
+{
+    public function render()
+    {
+        $produto = new Produto;
+        $produto->descricao = 'Caixa Chocolate Nestle';
+        $produto->valor = 12.39;
+        $produto->estoque = 30;
+        $this->printProduto($produto);
+        $produto->venda(5);
+        $this->printProduto($produto);
+        
+        // Produto 2
+        
+        $produto2 = new Produto;
+        $produto2->descricao = 'Cajuzinho Nestle';
+        $produto2->valor = 0.5;
+        $produto2->estoque = 15;
+  
+        $this->printProduto($produto2);
+        $produto2->atualizarPreco(50);
+        $this->printProduto($produto2);
+    }
 
-$produto2 = new Produto;
+    public function printProduto(Produto $p){
+        echo "<h3>{$p->descricao}</h3>";
+        echo "<h5>Valor: {$p->valor}</h5>";
+        echo "<h5>Quantidade: {$p->estoque}</h5><hr>";
+    }
+}
 
-$produto2->descricao = 'Cajuzinho Nestle';
-$produto2->valor = 0.5;
-$produto2->estoque = 15;
 
-echo '<pre>';
-var_dump($produto);
-var_dump($produto2);
+$pagina = new Pagina();
+$pagina->render();
