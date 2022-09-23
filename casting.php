aa2792544e12bb553a1c17b61e81b611081da67a
Joaquim Martins Scavone <joaquim.scavone@ifto.edu.br>
2022-09-23 16:00:08 -0300

    Aula 05

---
+++ casting.php
@@ -0,0 +1,24 @@
+<?php
+
+$obj = new stdClass;
+$obj->nome = 'Joaquim';
+$obj->idade = '99';
+$obj->sexo = 'M';
+
+$obj = (array) $obj;
+
+$array = [
+    'nome' => 'Miriam',
+    'idade' => 65,
+    'sexo' => 'F'
+];
+
+$obj2 = (object) $array;
+
+$obj3 = (object)$_GET;
+
+
+echo '<pre>';
+print_r($obj);
+print_r($obj2);
+print_r($obj3->nome);
